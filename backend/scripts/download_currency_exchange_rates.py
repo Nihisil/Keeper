27c8c01ed63c95f0cea8d90e1773105545ff7979
Nihisil <alexey@nihisil.com>
2021-04-07 10:37:29 +0800

    [Finance/Currency] Change currency exchange rates provider

--- backend/scripts/download_currency_exchange_rates.py
+++ backend/scripts/download_currency_exchange_rates.py
@@ -7,7 +7,6 @@ import requests
 from lib.finance.constants import MAIN_CURRENCY, OTHER_CURRENCIES, Currency
 from lib.finance.currency_exchange_rates.crud import (
     create_currency_exchange_rate,
-    delete_all_currency_exchange_rates,
     get_currency_exchange_rate_for_date,
 )
 from lib.finance.currency_exchange_rates.models import CurrencyExchangeRate
@@ -15,7 +14,7 @@ from lib.logger import set_up_logging
 from lib.sentry import init_sentry
 
 DATE_FORMAT = "%Y-%m-%d"
-BASE_URL = "https://api.exchangeratesapi.io"
+BASE_URL = "https://api.exchangerate.host"
 
 init_sentry()
 logger = set_up_logging()
@@ -23,53 +22,26 @@ logger = set_up_logging()
 
 def main():
     parser = argparse.ArgumentParser(description="Download currency exchance rates")
-    parser.add_argument("--history", dest="history", action="store_true")
+    parser.add_argument("--date", dest="date")
     args = parser.parse_args()
 
-    if args.history:
-        # it is important to clear existing data before download historic data
-        delete_all_currency_exchange_rates()
-        download_history_data()
-    else:
-        download_daily_update()
+    date = datetime.utcnow().strftime(DATE_FORMAT)
+    if args.date:
+        date = args.date
+
+    download_daily_update(date)
 
 
-def download_history_data():
-    logger.info("Starting historical currency data downloading")
-
-    start_date = datetime(2015, 1, 1).strftime("%Y-%m-%d")
-    end_date = datetime.utcnow().strftime(DATE_FORMAT)
+def download_daily_update(date: datetime):
+    logger.info(f"Starting currency data downloading on {date}")
 
     data = requests.get(
-        f"{BASE_URL}/history",
-        params={
-            "start_at": start_date,
-            "end_at": end_date,
-            "base": MAIN_CURRENCY.value,
-            "symbols": ",".join(OTHER_CURRENCIES),
-        },
-    ).json()["rates"]
-
-    for date, rates in data.items():
-        date = datetime.strptime(date, DATE_FORMAT)
-        _insert_currency_record(date, rates)
-
-    logger.info("Finishing historical data downloading")
-
-
-def download_daily_update():
-    logger.info("Starting daily currency data downloading")
-
-    data = requests.get(
-        f"{BASE_URL}/latest",
+        f"{BASE_URL}/{date}",
         params={
             "base": MAIN_CURRENCY.value,
             "symbols": ",".join(OTHER_CURRENCIES),
         },
     ).json()
-    date = data["date"]
-
-    logger.info(f"Found new data on {date}")
 
     date = datetime.strptime(date, DATE_FORMAT)
     _insert_currency_record(date, data["rates"])
