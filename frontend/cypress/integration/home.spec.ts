bbbde782e9d916453c2aa7c2626cedaeb883f845
Nihisil <alexey@nihisil.com>
2021-04-12 18:37:29 +0800

    [Accounts] Rebuild Login form with formik for better errors validation

--- frontend/cypress/integration/home.spec.ts
+++ frontend/cypress/integration/home.spec.ts
@@ -1,6 +1,6 @@
 context("Home page", () => {
   it("shows login form by default", () => {
     cy.visit("/");
-    cy.get("[data-cy=login-header]").invoke("text").should("equal", "Please log in");
+    cy.get("[data-cy=login-header]").invoke("text").should("equal", "Sign in");
   });
 });
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,68 +1,122 @@
 import "components/Login/Login.scss";
 
 import { Token } from "client/data-contracts";
-import React, { useState } from "react";
+import { Field, FieldProps, Form, Formik, FormikHelpers, FormikProps } from "formik";
+import React from "react";
 import api from "utils/api";
+import { object, string } from "yup";
+
+interface Values {
+  username: string;
+  password: string;
+}
+
+const validationSchema = object({
+  username: string().required("Username is required."),
+  password: string().required("Enter your password."),
+});
 
 interface LoginProps {
   setToken(userToken: Token): void;
 }
 
 export default function Login({ setToken }: LoginProps): JSX.Element {
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [validationError, setValidationError] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const handleSubmit = async (values: Values, actions: FormikHelpers<Values>) => {
     try {
-      const response = await api.auth.authenticate({ username, password });
+      const response = await api.auth.authenticate({ username: values.username, password: values.password });
       setToken(response.data);
+      actions.setSubmitting(false);
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        setValidationError("Incorrect login or password.");
+        actions.setStatus("Incorrect login or password.");
+        actions.setSubmitting(false);
       } else {
         throw error;
       }
     }
   };
 
-  let errorBlock = null;
-  if (validationError) {
-    errorBlock = <p className="text-danger">{validationError}</p>;
-  }
-
   return (
     <>
       <main role="main" className="container">
         <div className="row justify-content-md-center">
           <div className="col-md-5 text-center">
-            <form className="login-form" onSubmit={handleSubmit}>
+            <div>
               <h2 className="h3 mb-3 font-weight-normal" data-cy="login-header">
-                Please log in
-              </h2>
-              {errorBlock}
-              <input
-                type="text"
-                id="inputUsername"
-                className="form-control"
-                placeholder="Username"
-                required
-                onChange={(e) => setUserName(e.target.value)}
-              />
-              <input
-                type="password"
-                id="inputPassword"
-                className="form-control"
-                placeholder="Password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button className="btn btn-md btn-primary btn-block" type="submit">
                 Sign in
-              </button>
-            </form>
+              </h2>
+
+              <Formik
+                validationSchema={validationSchema}
+                initialValues={{
+                  username: "",
+                  password: "",
+                }}
+                onSubmit={(values: Values, actions: FormikHelpers<Values>) => {
+                  handleSubmit(values, actions);
+                }}
+              >
+                {(props: FormikProps<Values>) => (
+                  <Form>
+                    {props.status && (
+                      <div className="alert alert-danger" role="alert">
+                        {props.status}
+                      </div>
+                    )}
+
+                    <div className="form-group">
+                      <Field name="username">
+                        {({ field, meta }: FieldProps) => (
+                          <>
+                            <div>
+                              <input
+                                type="text"
+                                className={`form-control ${meta.error ? "is-invalid" : ""}`}
+                                placeholder="Username"
+                                name={field.name}
+                                value={field.value}
+                                onChange={field.onChange}
+                              />
+                              {meta.touched && meta.error && (
+                                <div className="invalid-feedback">{meta.error}</div>
+                              )}
+                            </div>
+                          </>
+                        )}
+                      </Field>
+                    </div>
+
+                    <div className="form-group">
+                      <Field name="password">
+                        {({ field, meta }: FieldProps) => (
+                          <>
+                            <div>
+                              <input
+                                type="password"
+                                className={`form-control ${meta.error ? "is-invalid" : ""}`}
+                                placeholder="Password"
+                                name={field.name}
+                                value={field.value}
+                                onChange={field.onChange}
+                              />
+                              {meta.touched && meta.error && (
+                                <div className="invalid-feedback">{meta.error}</div>
+                              )}
+                            </div>
+                          </>
+                        )}
+                      </Field>
+                    </div>
+
+                    <div className="form-group">
+                      <button type="submit" className="btn btn-primary" disabled={props.isSubmitting}>
+                        Sign in
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
           </div>
         </div>
       </main>
